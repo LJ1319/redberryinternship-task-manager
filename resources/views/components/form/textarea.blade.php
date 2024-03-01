f0d12ac8f9338ba1d8d675de25c0d1c20026ac5c
LJ1319 <luka.jikia.2@btu.edu.ge>
2024-03-01 13:02:20 +0400

    style: add minor markup changes

--- resources/views/components/form/textarea.blade.php
+++ resources/views/components/form/textarea.blade.php
@@ -5,6 +5,6 @@
     id="{{ $name }}"
     placeholder="{{ ucfirst(__("messages.{$message}")) }}"
     required
-    class="h-40 w-full p-4 rounded-[14px] bg-[#F6F8FA] focus:outline-none focus:ring-1 focus:ring-[#499AF9] @if($errors->get($name)) border border-red-500 focus:ring-opacity-0 @endif"
+    class="h-40 w-full p-4 rounded-[14px] bg-[#F6F8FA] focus:outline-none focus:ring-1 focus:ring-[#499AF9] @if($errors->get($error)) border border-red-500 focus:ring-opacity-0 @endif"
     {{ $attributes }}
 >{{ old($error) }}</textarea>
